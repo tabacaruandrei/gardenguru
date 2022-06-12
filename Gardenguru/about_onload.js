7e87667e44d49aebcf8ded8d587585ac62342cd5
tabacaruandrei <35452862+tabacaruandrei@users.noreply.github.com>
2022-06-12 22:30:30 +0300

    Delete Gardenguru directory

--- Gardenguru/about_onload.js
+++
@@ -1,9 +0,0 @@
-function aboutFadeIn(){
-    const fader = document.querySelector('.fade-in');
-    fader.classList.toggle('appear');
-  }
-
-window.addEventListener('load',
-function() {
-    aboutFadeIn();
-}, false);
